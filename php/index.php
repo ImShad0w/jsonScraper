6ee2cfc5dbe05b37c30c0bdb9451638f4923d4d9
shad0w <kirillpashnin.dev@gmail.com>
2025-09-14 17:06:24 +0200

    Added books json and parsed the books into the html

--- php/index.php
+++ php/index.php
@@ -16,19 +16,21 @@
     return "rgb($r, $g, $b)";
 }
     $json = file_get_contents('data.json'); 
+    $json2 = file_get_contents('boos.json');
 
     if ($json === false) {
         die('Error reading the JSON file');
     }
 
     $json_data = json_decode($json, true); 
+    $books = json_decode($json2, true);
 
     if ($json_data === null) {
         die('Error decoding the JSON file');
     }
 
     //print_r($json_data);
-
+    echo "<h1>Movies</h1>";
     // Loop through all
     foreach ($json_data as $movie) {
         $color = randomPastelColor();
@@ -37,7 +39,16 @@
         echo "Year: " . $movie['year'] . "<br>";
         echo "</div>";
     }
-
+    
+    echo "<h1>Books</h1>";
+    foreach ($books as $book) {
+        $color = randomPastelColor();
+        echo '<div class="item" style="background-color:' . $color . ';">';
+        echo "Title: " . $book['title'] . "<br>";
+        echo "Year: " . $book['year'] . "<br>";
+        echo "Author: " . $book['author'] . "<br>";
+        echo "</div>";
+    }
     ?>
 </body>
 </html>
